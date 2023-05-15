92f3f7517cd522b7243033503e563e8d51eddd84
masomdewan24 <masom24@gmail.com>
2023-05-15 21:38:29 +0600

    end of 61.3

--- index.js
+++ index.js
@@ -6,12 +6,41 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 
+const news = require('./data/news.json');
+
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
 
 app.get('/new-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 8) {
+        res.send(news)
+    }
+    else {
+        const seletedNews = news.find(n => n._id === id)
+    res.send(seletedNews)
+    }  
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "8") {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews);
+    }
+   
+    
 })
 app.listen(port, () => {
     console.log( 'Dragon News Server running Masum', port)
